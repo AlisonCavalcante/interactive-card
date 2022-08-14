85632ab665067102ed47f1c575c642387edd1b75
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-08-14 13:26:07 -0300

    first commit

---
+++ interactive-card/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormComponent } from './components/form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
